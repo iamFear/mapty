6aff69e69764ee580dd4eee8ea9216a3ba51d8ff
Daniel Burgoa <danielburgoa50@gmail.com>
2024-10-08 14:00:37 -0500

    classes added

--- script.js
+++ script.js
@@ -14,9 +14,50 @@ let inputCadence = document.querySelector('.form__input--cadence');
 let inputElevation = document.querySelector('.form__input--elevation');
 
 // OOP
+class Workout {
+  date = new Date();
+  id = Date.now();
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in KM
+    this.duration = duration; // in Minutes
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // Cadence = Duration / Distance
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// /////////////////////////////////////////////////////////////////////////////
+// Application Architecture
 class App {
   #map;
   #mapEvent;
+
   constructor() {
     this._getPosition();
 
